d39297afa60234d90fd5b742e7bff93dff47b054
adminuser <adminuser@kali.mvm>
2022-05-27 10:57:26 +0530

    initial2

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static("./public"))
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html')
+})
+
+
+app.listen(port = 2000, () => {
+    console.log("Server is running on port 2000")
+})
