8fcf5c82366ea44a0f61bbfdd84001d6da92b44a
adminuser <adminuser@kali.mvm>
2022-05-27 12:11:34 +0530

    inital5

--- public/service_worker.js
+++ public/service_worker.js
@@ -3,9 +3,9 @@
 // events.
 // when we say self. it is specific to the curren file
 
-const cache = "v1"
+const cache_name = "v1"
 
-const caching_files = [
+const caching_assets = [
     'index.html',
     'style.css',
     'index.js'
@@ -19,8 +19,10 @@ self.addEventListener("install", (event) => {
         
         (async() => {
             try {
-                cache_obj = await caches.open(cache)
-                cache_obj.addAll(caching_files)
+                const cache_obj = await caches.open(cache_name);
+                cache_obj.addAll(caching_assets);
+
+                const skip = self.skipWaiting();
             }
             catch{
                 console.log("error occured while caching...")
@@ -32,11 +34,39 @@ self.addEventListener("install", (event) => {
 
 
 // activated event
+// before activating the service worker, we can get rid of the old cache.
 self.addEventListener("activate", (event) => {
     console.log("Service Worker : Activated!")
+
+    // removing the old cache.
+    event.waitUntil(
+
+        (async () => {
+
+            const cache_keys = await caches.keys()
+            console.log(cache_keys)
+
+            cache_keys.forEach(
+                key => {
+                    if (key !== cache_name) {
+                        console.log("Service Worker deleted old cache!")
+                        return caches.delete(key)
+                        
+                    }
+                }
+            )
+            return Promise.all(cache_keys)
+
+
+        })()
+
+    )
 })
 
 // In the fetch event, we need to configure that, whenever the browser
 // try to get the file from the server, we need to check whether the broswer
 // is online or offline. and give the files according to it.
 
+self.addEventListener("fetch", (event) => {
+    console.log("Service Worker : fetch!")
+})
